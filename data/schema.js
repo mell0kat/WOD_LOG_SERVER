ca1ec8f22cf4a134ac098d71bda6644e8fab3667
Katherine Mello <katherine.mello8@gmail.com>
2017-12-17 11:26:04 -0500

    Initial commit

--- data/schema.js
+++ data/schema.js
@@ -1,14 +1,31 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import mocks from './mocks';
+import resolvers from './resolvers'
 
 const typeDefs = `
-type Query {
-  testString: String
-}
+
+	type Query {
+		author(firstName: String, lastName: String): Author
+		getFortuneCookie: String
+	}
+	type Author {
+	  id: Int
+	  firstName: String
+	  lastName: String
+	  posts: [Post]
+	}
+
+	type Post {
+		id: Int
+		title: String
+		text: String
+		views: Int
+		author: Author
+	}
 `;
 
-const schema = makeExecutableSchema({ typeDefs });
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-addMockFunctionsToSchema({ schema, mocks });
+// addMockFunctionsToSchema({ schema, mocks });
 
 export default schema;
